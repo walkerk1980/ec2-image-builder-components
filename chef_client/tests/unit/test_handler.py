454fbd1e57831ab17ce6b0e45e58e363ee0156b7
Keith Walker <walkerk1980@gmail.com>
2020-04-27 23:24:35 +0000

    initial chef_client code

---
+++ chef_client/tests/unit/test_handler.py
@@ -0,0 +1,14 @@
+import json
+
+import pytest
+
+from component_files import app
+
+def test_lambda_handler():
+
+    data = json.loads(ret["body"])
+
+    assert ret["statusCode"] == 200
+    assert "message" in ret["body"]
+    assert data["message"] == "hello world"
+    # assert "location" in data.dict_keys()
